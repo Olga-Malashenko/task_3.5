3731e39cde983c117ace300585f3b1c7ee1f2e8a
Olga Malashenko <olga_birjukova@mail.ru>
2022-02-17 11:15:37 +0300

    Files Book.java and Smartphone.java created

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Smartphone extends Product {
+    private String firm;
+
+    public Smartphone() {
+        super();
+    }
+
+    public Smartphone(int id, String name, int price, String firm) {
+        super(id, name, price);
+        this.firm = firm;
+    }
+
+    public String getFirm() {
+        return firm;
+    }
+
+    public void setFirm(String firm) {
+        this.firm = firm;
+    }
+}
