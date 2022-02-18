816e2db23698f17a9d33a4e6e165b81c9c39dceb
Olga Malashenko <olga_birjukova@mail.ru>
2022-02-18 23:05:53 +0300

    Files SmartphoneTest.java and dataSmartphone.csv added

---
+++ src/test/java/ru/netology/domain/SmartphoneTest.java
@@ -0,0 +1,28 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmartphoneTest {
+    Product first = new Book(1, "Война и мир", 540, "Толстой");
+    Product second = new Book(2, "Братья Карамазовы", 480, "Достоевский");
+    Product third = new Book(3, "Мцыри", 130, "Лермонтов");
+    Product fourth = new Book(4, "Бесы", 256, "Достоевский");
+    Product fifth = new Book(5, "Преступление и наказание", 615, "Достоевский");
+    Product sixth = new Smartphone(6, "Galaxy M12", 14000, "Samsung");
+    Product seventh = new Smartphone(7, "Galaxy A22", 21000, "Samsung");
+    Product eighth = new Smartphone(8, "iPhone 13", 70000, "Apple");
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataSmartphone.csv")
+    void shouldByFirmAtMatches(String search, boolean expected) {
+        Product[] repo = {first,second,third,fourth,fifth,sixth,seventh,eighth};
+
+        boolean actual = seventh.matches(search);
+
+        assertEquals(expected, actual);
+    }
+}
