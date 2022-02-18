c02584a9b21d56f1a7a15ed6eab015a9869512b1
Olga Malashenko <olga_birjukova@mail.ru>
2022-02-18 22:56:11 +0300

    Files BookTest.java and dataBook.csv added

--- src/test/java/ru/netology/domain/BookTest.java
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProductTest {
-
+class BookTest {
     Product first = new Book(1, "Война и мир", 540, "Толстой");
     Product second = new Book(2, "Братья Карамазовы", 480, "Достоевский");
     Product third = new Book(3, "Мцыри", 130, "Лермонтов");
@@ -16,16 +15,14 @@ class ProductTest {
     Product sixth = new Smartphone(6, "Galaxy M12", 14000, "Samsung");
     Product seventh = new Smartphone(7, "Galaxy A22", 21000, "Samsung");
     Product eighth = new Smartphone(8, "iPhone 13", 70000, "Apple");
-    Product ninth = new Product(9, "Teddy bear", 1700);
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/dataProduct.csv")
-    void shouldByNameAtMatches(String search, boolean expected) {
+    @CsvFileSource(resources = "/dataBook.csv")
+    void shouldByAuthorAtMatches(String search, boolean expected) {
         Product[] repo = {first,second,third,fourth,fifth,sixth,seventh,eighth};
 
-        boolean actual = first.matches(search);
+        boolean actual = fifth.matches(search);
 //        boolean expected = true;
         assertEquals(expected, actual);
     }
 }
-
